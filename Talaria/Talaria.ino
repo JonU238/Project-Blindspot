24bdfee8b8bf9da752911541802dcc2f34956345
JonU238 <Jamestfishes@gmail.com>
2024-04-12 22:44:43 -0400

    Added compass and dropping stuff

--- Talaria/Talaria.ino
+++ Talaria/Talaria.ino
@@ -2,13 +2,17 @@
 #include <TinyGPS++.h>
 #include <math.h>
 #include <Servo.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_HMC5883_U.h>
 //Define Variables we'll be connecting to
 double Setpoint, Input, Output;
 
 //Specify the links and initial tuning parameters
 double Kp=2, Ki=5, Kd=1;
 PID SteerPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+
 TinyGPSPlus gps;
+
 struct WayPoint
 {
   float lat;
@@ -23,15 +27,28 @@ struct GpsData {
 };
 
 Servo steerServo;
+Servo dropServo;
 
 WayPoint destination;
+//CompassVVV
+Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified();
+
+//Dropper values
+  int dropALT = 1000;
+  int openAngle = 180;
+  int closeAngle = 0;
+  bool droppingTime = false;
+
+
 void setup(){
   //Serial Setup
   Serial.begin(9600);
   Serial1.begin(9600);
 
-  //Steering servo
+  //Servo setip
   steerServo.attach(2);
+  dropServo.attach(3);
+
   //initialize the variables we're linked to
   Input = 0;
   Setpoint = 0;
@@ -39,8 +56,10 @@ void setup(){
   //turn the PID on
   SteerPID.SetMode(AUTOMATIC);
   //Program in location 45.81905218493044, -82.47359070390377
-  destination.lat = 45.80634;
+  destination.lat = 45.806;
   destination.lon = -82.477;
+
+  
   
 }
 
@@ -49,7 +68,19 @@ GpsData cData = GetGPSData();
 
 void loop(){
   cData = GetGPSData();
-  Input = (HeadingBetweenTwoCoordinates(cData.latitude,cData.longitude,destination.lat,destination.lon)-HeadingBetweenTwoCoordinates(cData.latitude,cData.longitude,oldData.latitude,oldData.longitude));
+  //Dropper detection
+  if(cData.alt>dropALT){
+    droppingTime = true;
+  }
+  if(droppingTime){
+    dropServo.write(openAngle);
+  }
+  else{
+    dropServo.write(closeAngle);
+  }
+//END DROPPER SECTION
+
+  Input = (HeadingBetweenTwoCoordinates(cData.latitude,cData.longitude,destination.lat,destination.lon)-compHeading());
   if(abs(Input)>180){Input = 360-Input;}
   Setpoint = 0;
   SteerPID.Compute();
@@ -103,4 +134,23 @@ float HeadingBetweenTwoCoordinates(float lat1,float lon1,float lat2,float lon2){
 
 float distanceBetweenTwoCoordinates(float lat1,float lon1,float lat2,float lon2){
   return(sqrt(pow(((lat2-lat1)*111319.4),2)+pow(((lon2-lon1)*111319.4),2)));
+}
+
+float compHeading(){
+  
+  sensors_event_t event; 
+  mag.getEvent(&event);
+  float heading = atan2(event.magnetic.y, event.magnetic.x);
+  float declinationAngle = 0.19;
+  heading += declinationAngle;
+  // Correct for when signs are reversed.
+  if(heading < 0){
+    heading += 2*PI;
+  }
+    
+  // Check for wrap due to addition of declination.
+  if(heading > 2*PI){
+    heading -= 2*PI;
+    }
+return heading;
 }
